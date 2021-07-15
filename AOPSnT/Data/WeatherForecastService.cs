47d2aeed696c424d343aabaabd228991505f752c
Tim <tim.barani@vizientinc.com>
2021-07-15 08:41:53 -0500

    turned service back on

--- AOPSnT/Data/WeatherForecastService.cs
+++ AOPSnT/Data/WeatherForecastService.cs
@@ -15,14 +15,14 @@ namespace AOPSnT.Data
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
             var rng = new Random();
-            //return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            //{
-            //    Date = startDate.AddDays(index),
-            //    TemperatureC = rng.Next(-20, 55),
-            //    Summary = Summaries[rng.Next(Summaries.Length)]
-            //}).ToArray());
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
 
-            throw new Exception();
+            //throw new Exception();
         }
     }
 }
